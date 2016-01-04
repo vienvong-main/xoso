bad7e61c11e3619f8a41ad6956f438bd36d7d7f9
vietpn <vietpnk53@gmail.com>
2016-01-04 12:40:09 +0700

    update

--- application/views/templates/score.php
+++ application/views/templates/score.php
@@ -3,12 +3,16 @@
         <div class="panel-heading">
             <h1 class="panel-title">
                 <i class="glyphicon glyphicon-book"></i>
-                Kết quả bóng đá
+                Kết quả bóng đá - LIVESCORE
             </h1>
         </div>
 
         <div class="panel-body">
-            <iframe src="http://android.livescore.com/" width="100%" height="1500" frameborder="0" marginheight="0" marginwidth="0" scrolling="yes" id="livescoreif"></iframe>
+            <div style="height:1500px;overflow:hidden;" id="score-container">
+                <div style="visibility:visible;margin-top:-76px;display:block;">
+                    <iframe src="http://android.livescore.com/" width="100%" height="1500" frameborder="0" marginheight="0" marginwidth="0" scrolling="yes" id="livescoreif"></iframe>
+                </div>
+            </div>
         </div>
     </div>
 </div>
