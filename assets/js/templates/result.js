fc0319822dd582fe8c9fe95b5764a1a87cd0273b
vietpn <vietpnk53@gmail.com>
2016-01-04 11:24:17 +0700

    update ketqua

--- assets/js/templates/result.js
+++ assets/js/templates/result.js
@@ -2,12 +2,21 @@
 
 xosoApp.controller('resultCtrl', function($scope, $filter) {
     $scope.init = function(){
-        $scope.region = "";
+        $scope.region = '';
         $scope.date = $filter('date')(new Date(),'dd/MM/yyyy');
     };
 
     $scope.search = function(){
-        console.log($scope.region);
-        console.log($scope.date);
+        var erros = [];
+        if($scope.region == null || $scope.region == ''){
+            erros.push('Chưa chọn khu vực')
+        }
+        if($scope.date == null || $scope.date == ''){
+            erros.push('Chưa chọn ngày')
+        }
+
+        if(erros.length > 0){
+            alert(erros);
+        }
     };
 });
